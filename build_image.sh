8c3820e9cb9b79ce1fb76825c16bdc000da0f68a
Бекет Ермеков <byermekov@Bekets-Laptop.local>
2025-04-23 22:26:11 +0800

    fix: make version latest

--- build_image.sh
+++ build_image.sh
@@ -1,5 +1,5 @@
 docker buildx create --use 
 docker buildx build \
   --platform linux/amd64,linux/arm64 \
-  -t apolitichen/fastapi-postgres-app:1.1 \
+  -t apolitichen/fastapi-postgres-app:latest \
   --push .  
