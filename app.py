40483cc2e1e982f2d08d44c7b1928d83c82c552c
Бекет Ермеков <byermekov@Bekets-Laptop.local>
2025-04-24 23:06:34 +0800

    fix: flake8 import error

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI
 from sqlalchemy import create_engine, Column, Integer, String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
