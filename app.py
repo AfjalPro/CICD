f025f976cc941e380ab219817ba21d184b90d045
Бекет Ермеков <byermekov@Bekets-Laptop.local>
2025-04-25 14:44:58 +0800

    blalba

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app = FastAPI()
 
 @app.get("/")
 async def read_root():
-    return {"Hello": "World - Beket - V5!"}
+    return {"Hello": "World - Beket - V6!"}
 
 @app.get("/items/")
 async def read_items():
